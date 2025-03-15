bb2e47b90168c44ab89776048c9073c841895cab
Asdfobuy <106447745+Asdfobuy@users.noreply.github.com>
2025-03-15 19:01:14 +0100

    trying static page with public directory

--- server.js
+++ server.js
@@ -1,12 +1,21 @@
 const express = require('express');
 const app = express();
+const path = require('path');
+const fs = require('fs');
 
 // Use environment variable or default to port 3000
 const port = process.env.PORT || 3000; 
 
 app.use(express.urlencoded({ extended: true}))
 
-app.listen(port, () => {
-    console.log(`Server listening on http://localhost:${port}`);
-  });
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+app.listen(port, '0.0.0.0', () => {
+  console.log(`Server listening on http://0.0.0.0:${port}`);
+});
   
