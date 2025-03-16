a95da875d11470e39a37e238da332a2248fa43e0
ggombas36 <113374490+ggombas36@users.noreply.github.com>
2025-03-16 19:55:32 +0100

    create home.html & images site

---
+++ images.js
@@ -0,0 +1,5 @@
+const loadImages = () => {
+
+};
+
+export default loadImages;
--- server.js
+++ server.js
@@ -9,7 +9,11 @@ const port = process.env.PORT || 3000;
 app.use(express.urlencoded({ extended: true}))
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.sendFile(path.join(__dirname, 'public', "home.html"));
+})
+
+app.get('/images', (req, res) => {
+  res.send('Images!')
 })
 
 app.use(express.static(path.join(__dirname, 'public')));
@@ -18,7 +22,13 @@ app.get('/in', function (req,res, next) {
   res.render('home');
 })
 
-app.listen(port, '0.0.0.0', () => {
-  console.log(`Server listening on http://0.0.0.0:${port}`);
+// app.listen(port, '0.0.0.0', () => {
+//   console.log(`Server listening on http://0.0.0.0:${port}`);
+// });
+
+app.listen(port, () => {
+
 });
+
+
   
